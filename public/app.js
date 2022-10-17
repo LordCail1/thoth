5cf6b3723a616866afc356632d4fa7b76a453a91
LordCail1 <rami.batnigi@gmail.com>
2022-10-17 13:35:41 -0400

    Made it work

--- public/app.js
+++ public/app.js
@@ -9,7 +9,42 @@ generateCardsBtn.addEventListener("click", (e) => {
     layOutCard().catch(error => console.log("ERROR"));
 });
 
+
+async function layOutCard() {
+    //getting card
+    const cardResponse = await fetch("http://localhost:5000/getAllCards", {
+        method: "GET",
+        headers: {
+            "Content-Type":"application/json"
+        }
+    });
+    const card = await cardResponse.json();
+    
+    //getting image
+    const imageResponse = await fetch(`http://localhost:5000/image?imageName=${card.imageName}`);
+    const image = await imageResponse;
+    
+    addNewCard(card);
+    addNewImage(image);
+    console.log(image);
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
 function addNewCard(card) {
+    console.log(card);
     const newDiv = document.createElement("div");
     for (let property in card) {
         if (property === "link") {
@@ -30,24 +65,14 @@ function addNewCard(card) {
     mainContainer.appendChild(newDiv);
 }
 
-async function layOutCard() {
-    const response = await fetch("http://localhost:5000/getAllCards", {
-        method: "GET",
-        headers: {
-            "Content-Type":"application/json"
-        }
-    });
-    const data = await response.json();
-    addNewCard(data);
+function addNewImage(image) {
+    const newDiv = document.createElement("div");
+    const newImg = document.createElement("img");
+    newImg.src = image.url;
+    console.log(newImg);
+    mainContainer.lastChild.appendChild(newImg);
+
 }
-
-
-
-
-
-
-
-
 
 
 
