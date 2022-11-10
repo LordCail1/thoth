025ec1bd4fc25d1c43fa9b463faa0f7112a50946
LordCail1 <rami.batnigi@gmail.com>
2022-11-10 11:03:51 -0500

    .

---
+++ public/javascript/controllers/generateCards.js
@@ -0,0 +1,44 @@
+
+
+
+
+
+generateCardsBtn.addEventListener('click', (e) => {
+    if (pulledCards.length === 20) {
+        alert('You have already pulled 20 cards!');
+        return;
+    }
+    singeCard();
+});
+
+
+
+async function singeCard() {
+    try {
+        const cardResponse = await fetch('http://localhost:5000/cards/card', { 
+        method: 'GET',
+        headers: {
+            "Content-Type":"application/json" 
+        }});
+        const card = await cardResponse.json();
+        if (!card) return;
+        pulledCards.push(card);
+        cardOnUI(card);
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+
+
+function cardOnUI(card) {
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('cardContainer');
+    for (let property in card) {
+        if (property === "imageName") {
+            cardContainer.style.backgroundImage = `url('../../images/${card[property]}.jpg')`
+        }
+    }
+    cardTable.appendChild(cardContainer);
+    moveBehaviorToCards();
+}
