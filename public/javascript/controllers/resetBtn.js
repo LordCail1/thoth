6f9659ac639175f446309dd71f1d1f55b96fea9f
LordCail1 <rami.batnigi@gmail.com>
2022-11-12 08:20:02 -0500

    Update resetBtn.js

--- public/javascript/controllers/resetBtn.js
+++ public/javascript/controllers/resetBtn.js
@@ -1,24 +1,38 @@
 resetBtn.addEventListener('click', (e) => {
-    reset();
+    fetchCards();
     deleteAllCardsFromScreen();
+    resetStats();
 });
 
 
-async function reset() {
+async function fetchCards() {
     pulledCards = [];
     try {
         const response = await fetch('http://localhost:5000/cards/resetCards', {
-            method: 'GET',
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-    } catch (err) {
-        console.log(err.message);
-    }
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+} catch (err) {
+    console.log(err.message);
+}
 }
 
 function deleteAllCardsFromScreen() {
     // const cards = document.querySelectorAll('.cardContainer');
     cardTable.innerHTML = "";
+}
+
+function resetStats() {
+    const fireSpan = document.querySelector('[data-fire-perc]');
+    const waterSpan = document.querySelector('[data-water-perc]');
+    const airSpan = document.querySelector('[data-air-perc]');
+    const earthSpan = document.querySelector('[data-earth-perc]');
+
+    fireSpan.innerText = '0%';
+    waterSpan.innerText = '0%';
+    airSpan.innerText = '0%';
+    earthSpan.innerText = '0%';
+    
 }
